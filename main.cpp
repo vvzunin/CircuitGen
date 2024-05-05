0e21cf4e92bad1ca595a969b095fea81a63a14bf
Fuuulkrum7 <ilka747428@gmail.com>
2024-05-05 14:53:07 +0300

    Add help command

--- main.cpp
+++ main.cpp
@@ -19,13 +19,13 @@ int main(int argc, char **argv) {
   const std::string defaultLibPath = "tech_libs/";
   // Use getopt to parse command line arguments
 
-  const char *const short_opts = "j:rbtl:n:";
+  const char *const short_opts = "hj:rbtl:";
   const option long_opts[] = {{"json_path", required_argument, nullptr, 'j'},
                               {"make_resyn2", no_argument, nullptr, 'r'},
                               {"make_balance", no_argument, nullptr, 'b'},
                               {"to_bench", no_argument, nullptr, 't'},
-                              {"lib_name", required_argument, nullptr, 'l'},
-                              {"num_nodes", required_argument, nullptr, 'n'}};
+                              {"help", no_argument, nullptr, 'h'},
+                              {"lib_name", required_argument, nullptr, 'l'}};
   int c;
 
   int opt;
@@ -44,6 +44,35 @@ int main(int argc, char **argv) {
       case 't':
         toBench = true;
         break;
+      case 'h':
+        std::cout << "---------------------------------------------------"
+                  << std::endl;
+        std::cout << "CircuitGenGenerator util.\nProvides possibility to "
+                     "generate verolog circuits."
+                  << std::endl;
+        std::cout << "---------------------------------------------------"
+                  << std::endl;
+        
+        std::cout << "Options: " << std::endl;
+        std::cout << "\t-j <path> or --json_path <path>:\n\t\tUsed for setting path "
+                     "to json file with circuit parameters."
+                  << std::endl;
+        std::cout << "\t-b or --make_balance:\n\t\tRuns Berkeley-abc to optimize "
+                     "generated files with\n\t\tbasic balance commands."
+                  << std::endl;
+        std::cout << "\t-r or --make_resyn2:\n\t\tRuns Berkeley-abc to optimize "
+                     "generated files with\n\t\toptimization algorythm named resyn2."
+                  << std::endl;
+        std::cout << "\t-t or --to_bench:\n\t\tRuns Berkeley-abc to convert Verilog "
+                     "file to Bench format."
+                  << std::endl;
+        std::cout << "\t-l <lib_name> or --lib_name <lib_name>:\n\t\tSets lib, used "
+                     "by Berkeley-abc (default sky130.lib).\n\t\tFile should be "
+                     "located in the folder tech_libs."
+                  << std::endl;
+        std::cout << "\t-h or --help:\n\t\tShows all commands being used."
+                  << std::endl;
+        return 0;
       case 'l':
         libName = optarg;
         break;
