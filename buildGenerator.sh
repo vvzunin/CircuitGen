ecd3843eb89bd9ce4de0d7ed83bd889acb09e6d9
Vladimir Zunin <vzunin@hse.ru>
2024-12-07 14:25:24 +0300

    Fix buildGenerator.sh

--- buildGenerator.sh
+++ buildGenerator.sh
@@ -3,5 +3,5 @@ if [ ! -d "Generator/build" ]; then
   mkdir Generator/build
 fi
 
-cmake -S . -B Generator/build -G "Unix Makefiles"
+cmake -S Generator -B Generator/build -G "Unix Makefiles"
 cmake --build Generator/build -j $(nproc)
