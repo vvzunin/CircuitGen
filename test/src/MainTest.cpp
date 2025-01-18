63421d74e8c6a6a6f88ab1add81957c455b30744
Vasilisa Kudriavtseva <Phoenix.aka.Feniks@yandex.ru>
2025-01-18 12:07:08 +0300

    added fail test

---
+++ test/src/MainTest.cpp
@@ -0,0 +1,19 @@
+#include <filesystem>
+
+#include "main.cpp"
+
+#include "GraphAbcFolder.hpp"
+
+#include <gtest/gtest.h>
+
+//#include "easylogging++Init.hpp"
+#include "fstream"
+
+const std::string testDirectory = std::filesystem::current_path();
+
+TEST(Circuit, FailTest){
+  
+    EXPECT_EQ(0, 1);
+    std::cout<<"Fail";
+}
+
