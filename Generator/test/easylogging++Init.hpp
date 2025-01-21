b6b2d4f63d938bd74d4ded4a90064f63a6ede991
Vasilisa Kudriavtseva <Phoenix.aka.Feniks@yandex.ru>
2025-01-21 15:48:14 +0300

    new version of test build

--- Generator/test/easylogging++Init.hpp
+++ Generator/test/easylogging++Init.hpp
@@ -7,9 +7,10 @@ using namespace std;
 void initLogging(string testName, string subtestName) {
   el::Configurations conf("easylogging++.conf");
   el::Loggers::reconfigureAllLoggers(conf);
-  el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Filename, "logs/"+ testName + "/" + subtestName + ".log");
+  el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Filename,
+                                     "logs/" + testName + "/" + subtestName +
+                                         ".log");
   el::Loggers::addFlag(el::LoggingFlag::NewLineForContainer);
   el::Loggers::addFlag(el::LoggingFlag::LogDetailedCrashReason);
   el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
-  // LOG(INFO) << "Logging started\n\n";
 }
